0ea2df40fe254aaf32338f54c88593beb38468ae
Leozin0201 <106625293+Leozin0201@users.noreply.github.com>
2022-06-01 14:03:57 -0300

    LEONARDO MISSIAGIA MUITO BRABO O CARA É BOM

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 console.log("Rodando o sistema de gerenciamento de Jogos") 
 console.log("Aplicação esta se conectando ao Banco de dados")
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meus jogos</title>
+</head>
+<body>
+    <h1>Sistema de cadastro de jogos</h1>
+    <h2>Leonardo é um eximio suporte técnico, muito bom no que ele faz, deveriam dar um aumento de salario pra ele, o cara é sensacional</h1>
+</body>
+</html>
